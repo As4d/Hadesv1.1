b45fa4d7056cd32b0579f1f776a44ad6b1e677a7
Asad <asadkhan91003@gmail.com>
2021-10-24 20:01:41 +0100

    kivy update

--- kivyGUI.py
+++ kivyGUI.py
@@ -1,10 +1,27 @@
 import kivy
 from kivy.app import App
+from kivy.core import text
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
+class GridLayout(GridLayout):
+    
+    def __init__(self, **kwargs):
+        super(GridLayout, self).__init__(**kwargs)
+
+        self.cols = 2
+
+        self.add_widget(Label(text = "Whos better"))
+        self.add_widget(TextInput(multiline= False))
+
+        self.add_widget(Label(text = "W/L: "))
+        self.add_widget(TextInput(multiline= False))
 
 class Window(App):
+
     def build(self):
-        return Label(text = "Hello world ", font_size = 72)
+        return GridLayout()
 
 Window().run()
