53c1cecfe451745218e0e47cdce345006b0e4684
Asad <asadkhan91003@gmail.com>
2022-03-16 11:03:23 +0000

    slight changes to score

--- score.py
+++ score.py
@@ -1,3 +1,32 @@
 class Score:
-	def __init__(self):
-		self.flags = None
+    def __init__(self):
+        self.score = 100
+        self.colour = {
+            (0, 24): "Red",
+            (25, 49): "Orange",
+            (50, 74): "Yellow",
+            (75, 100): "Green",
+        }
+
+    def CalculateCoulour(self):
+        for range in self.colour:
+            if self.score >= range[0] and self.score <= range[1]:
+                return self.colour[range]
+
+    def CalculateAcessMetric(self):
+        pass
+
+    def CalculateIntegrityMetric(self):
+        pass
+
+    def CalculateConfidentialityMetric(self):
+        pass
+
+    def GetVulnerableFileNameFlags(self):
+        pass
+
+    def GetVulnerableTextInFileFlags(self):
+        pass
+
+    def GetPythonFilesAccessed(self):
+        pass
